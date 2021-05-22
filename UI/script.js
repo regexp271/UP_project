884abb79f84a1516d2273843971b9c9ab1b4d769
Anton <anton.rydzevsky@gmail.com>
2021-05-22 20:23:11 +0300

    Added NumberValidation and getAds

--- UI/script.js
+++ UI/script.js
@@ -77,6 +77,26 @@
     }
 
 
+    function NumberValidation (number, min, max) {
+        if (typeof number === "number") {
+            if (arguments.length >= 2) 
+                if (number < min) return false;
+
+            if (arguments.length >= 3) 
+                if (number > max) return false;
+
+            return true;
+        }
+        else return false;
+    }
+
+    console.log(NumberValidation("15"));
+    console.log(NumberValidation(15));
+    console.log(NumberValidation(15, 10));
+    console.log(NumberValidation(15, 20));
+    console.log(NumberValidation(15, 10, 20));
+
+
     adList = [
         {
             id: "1",
@@ -96,7 +116,7 @@
         {
             id: "2",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 2, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -111,9 +131,9 @@
         {
             id: "3",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
-            link: "https://azure.microsoft.com/",
-            vendor: "Microsoft Azure",
+            createdAt: new Date(2021, 0, 3, 12, 30, 0, 0),
+            link: "https://mrsnow.com/",
+            vendor: "Mr. Snow",
             photoLink: "pic.png",
             hashTags: [
                 "hashtag1", "hashtag2"
@@ -126,7 +146,7 @@
         {
             id: "4",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 4, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -141,7 +161,7 @@
         {
             id: "5",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 5, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -156,7 +176,7 @@
         {
             id: "6",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 6, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -171,9 +191,9 @@
         {
             id: "7",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
-            link: "https://azure.microsoft.com/",
-            vendor: "Microsoft Azure",
+            createdAt: new Date(2021, 0, 7, 12, 30, 0, 0),
+            link: "https://mr_snow/",
+            vendor: "Mr. Snow",
             photoLink: "pic.png",
             hashTags: [
                 "hashtag1", "hashtag2"
@@ -186,7 +206,7 @@
         {
             id: "8",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 8, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -201,7 +221,7 @@
         {
             id: "9",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 9, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -216,7 +236,7 @@
         {
             id: "10",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 10, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -246,7 +266,7 @@
         {
             id: "12",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 2, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -261,7 +281,7 @@
         {
             id: "13",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 3, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -276,9 +296,9 @@
         {
             id: "14",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
-            link: "https://azure.microsoft.com/",
-            vendor: "Microsoft Azure",
+            createdAt: new Date(2021, 0, 4, 12, 30, 0, 0),
+            link: "https://mrsnow.com/",
+            vendor: "Mr. Snow",
             photoLink: "pic.png",
             hashTags: [
                 "hashtag1", "hashtag2"
@@ -291,7 +311,7 @@
         {
             id: "15",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 5, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -306,9 +326,9 @@
         {
             id: "16",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
-            link: "https://azure.microsoft.com/",
-            vendor: "Microsoft Azure",
+            createdAt: new Date(2021, 0, 6, 12, 30, 0, 0),
+            link: "https://mrsnow.com/",
+            vendor: "Mr. Snow",
             photoLink: "pic.png",
             hashTags: [
                 "hashtag1", "hashtag2"
@@ -321,7 +341,7 @@
         {
             id: "17",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 7, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -336,7 +356,7 @@
         {
             id: "18",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 8, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -351,7 +371,7 @@
         {
             id: "19",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 9, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -366,7 +386,7 @@
         {
             id: "20",
             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            createdAt: new Date(2021, 0, 1, 12, 30, 0, 0),
+            createdAt: new Date(2021, 0, 10, 12, 30, 0, 0),
             link: "https://azure.microsoft.com/",
             vendor: "Microsoft Azure",
             photoLink: "pic.png",
@@ -381,5 +401,55 @@
     ];
 
 
-    
+    function getAds (skip, top, filterConfig) {
+        if (skip === undefined) skip = 0;
+        if (top === undefined) top = 10;
+        var resultArray = [];
+
+        for (var i = 0; i < adList.length; i++) {
+            var flag = true;
+
+            for (var str in filterConfig) {
+                if (str == "date") {
+                    if (filterConfig.date.since > adList[i].createdAt ||
+                        filterConfig.date.till < adList[i].createdAt)
+                        flag = false;
+                        break;
+                }
+                else {   
+                    if (adList[i][str] != filterConfig[str]) {
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+
+            if (flag) resultArray.push(adList[i]);
+        }
+        
+        resultArray.sort(function (a, b) {
+            return b.createdAt.getTime() - a.createdAt.getTime();
+        });
+
+        return resultArray.slice(skip, skip + top);
+    };
+
+    console.log(getAds());
+    console.log(getAds(0, 5));
+    console.log(getAds(5, 5));
+    console.log(getAds(0, 10, {
+        vendor: "Mr. Snow"
+    }));
+    console.log(getAds(0, 10, {
+        vendor: "Mr. Snow", 
+        link: "https://mrsnow.com/"
+    }));
+    console.log(getAds(0, 10, {
+        vendor: "Mr. Snow", 
+        link: "https://mrsnow.com/",
+        date: {
+            since: new Date(2021, 0, 3, 10),
+            till: new Date(2021, 0, 4, 20)
+        }
+    }));
 }());
